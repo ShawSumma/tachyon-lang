f446ca2281ce31c8892090c87a480fa8253cfe94
Xeno <shawsumma@gmail.com>
2018-03-21 00:48:53 -0400

    remove broken line count
    remove broken line count file from flat ion

--- core/line_count.py
+++
@@ -1,6 +0,0 @@
-import os
-lins = 0
-for i in os.listdir():
-    if i[0] not in '_.' and i != 'line_count.py':
-        lins += len(list(filter(lambda x: x.strip() != '' and x.strip()[0] != '#',open(i).read().split('\n'))))
-print(lins)
